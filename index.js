e264920370dba71a8167e5a071c5b2d790b26c39
hp-nyc <helenjpark@gmail.com>
2025-10-30 21:46:44 -0400

    javascript for securing the vault

---
+++ index.js
@@ -0,0 +1,22 @@
+var code1 = 10;
+// console.log(code1);
+
+var code1 = 7 + 3;
+// console.log(code1);
+
+let code2 = 100 / 2 - 10;
+// console.log(code2);
+
+let code3 = (3 % 2) + 38;
+
+// console.log(code3);
+
+let message = `The vault has been secured. The combination is:`;
+
+console.log(message);
+
+let codeA = code1 + "-" + code2 + "-" + code3;
+console.log(codeA);
+
+let codeB = `${code1}-${code2}-${code3}`;
+console.log(codeB);
